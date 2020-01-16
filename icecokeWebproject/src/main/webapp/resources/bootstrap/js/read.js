eb1299a64d8d13eaa7ce0be3144b78f94fdd5351
coke <coke@Laptop>
2020-01-16 09:24:38 +0900

    done  커밋 오류 다시 커밋 Todo  커밋 확인.

--- icecokeWebproject/src/main/webapp/resources/bootstrap/js/read.js
+++ icecokeWebproject/src/main/webapp/resources/bootstrap/js/read.js
@@ -23,7 +23,7 @@ var btndelete = document.getElementById("btndelete");
 btndelete.addEventListener("click", function(e){
 
 	// 여기 눌렀을때 진짜 삭제하는지 경고 메시지 .
-	//dialog 사용 할꺼이ㅣㅁ.
+	//dialog 사용 할꺼이.
 	
 	$.ajax({
 		// 삭제 POST로 보내는 거 짜야함.
