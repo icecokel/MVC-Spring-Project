0a3c7b11d00cfd95711b31934cc89ce69032392c
tjoeun304 <tjoeun304@218.38.137.28>
2020-02-05 18:11:39 +0900

    [Done]  1.xml 파일다운로드 까지 정상 확인. [ING]  1. git 꼬임 문제 유무 확인 진행 중 .  2. 파일 업도르 시 파일 업로드 문제 유무 확인 중.  3. DB 이동으로 인한 문제 유무 확인 중.  4. 게시판 에디터 변경 예정.  5. 결과값 앞단으로 파싱 진행중.
    [Todo]
     1. 게시글 textarea 높이 조절 필요, 타인 글 확인시 툴바 제거기능 여부 확인 필요.
    
    [ETC]
     *

--- icecokeWebproject/src/main/java/com/coke/ice/XMLContoller.java
+++ icecokeWebproject/src/main/java/com/coke/ice/XMLContoller.java
@@ -1,14 +1,28 @@
 package com.coke.ice;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.coke.ice.domain.XMLhani;
+
+
 
 @Controller
 public class XMLContoller {
@@ -16,7 +30,7 @@ public class XMLContoller {
 	@RequestMapping (value="/xml/hanirss" , method=RequestMethod.GET)
 	public String hanirss(Model model) {
 		String xml ="";
-
+		
 		// xml 다운로드 진행.
 		try {
 			// 한겨례 rss 주소,
@@ -56,17 +70,32 @@ public class XMLContoller {
 		
 		// xml 파싱
 		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
 			
+			InputStream is = new ByteArrayInputStream(xml.getBytes());
+			
+			// 메모리에 펼치기.
+			Document docu = db.parse(is);
+			
+			// 루트 찾아오기
+			Element root=docu.getDocumentElement();
+			
+			// title 태그 찾아오기
+			NodeList titles=root.getElementsByTagName("title");
+			
+			// title을 하나씩 가져와서 출력
+			for(int i=0; i<5; i++) {
+				Node node=titles.item(i);
+				Node nodetitle=node.getFirstChild();
+				System.out.println(nodetitle.toString());
+			}
 		} catch (Exception e) {
 			System.out.println("xml 파싱예외.");
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 			
 		}
-		
-		
-		
-		
 		
 		return "/xml/hanirss";
 		// 참고 소스 https://blog.naver.com/ffiwe/221687137070
---
+++ icecokeWebproject/src/main/java/com/coke/ice/domain/XMLhani.java
@@ -0,0 +1,10 @@
+package com.coke.ice.domain;
+
+import lombok.Data;
+
+@Data
+public class XMLhani {
+	private String title;
+	private String content;
+
+}
