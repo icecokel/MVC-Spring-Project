b0fc06671557e47c239db0efa66e1a9875816ae9
tjoeun304 <tjoeun304@218.38.137.28>
2020-02-05 17:35:54 +0900

    [Done]  1. XML 구현중. [ING]  1. git 꼬임 문제 유무 확인 진행 중 .  2. 파일 업도르 시 파일 업로드 문제 유무 확인 중.  3. DB 이동으로 인한 문제 유무 확인 중.  4. 게시판 에디터 변경 예정.  5. XML 컨트롤러 작성중.
    [Todo]
     1. 게시글 textarea 높이 조절 필요, 타인 글 확인시 툴바 제거기능 여부 확인 필요.
    
    
    [ETC]
     * 한겨례 xml 파싱 준비.

--- icecokeWebproject/src/main/java/com/coke/ice/XMLContoller.java
+++ icecokeWebproject/src/main/java/com/coke/ice/XMLContoller.java
@@ -1,5 +1,10 @@
 package com.coke.ice;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +15,61 @@ public class XMLContoller {
 	
 	@RequestMapping (value="/xml/hanirss" , method=RequestMethod.GET)
 	public String hanirss(Model model) {
+		String xml ="";
+
+		// xml 다운로드 진행.
+		try {
+			// 한겨례 rss 주소,
+			String addr = "http://www.hani.co.kr/rss/";
+			
+			URL url = new URL(addr);
+			
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			// 연결대기시간.
+			con.setConnectTimeout(30000);
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			
+			StringBuilder sb = new StringBuilder();
+			
+			while(true){
+				String line =br.readLine();
+				
+				if(line== null) {
+					break;
+				}
+				
+				sb.append(line + "\n");
+			}
+			
+			xml = sb.toString();
+			
+			br.close();
+			con.disconnect();
+			
+		}catch(Exception e) {
+			System.out.println("xml 다운로드 예외");
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			
+		}
+		
+		// xml 파싱
+		try {
+			
+		} catch (Exception e) {
+			System.out.println("xml 파싱예외.");
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			
+		}
+		
+		
+		
+		
 		
 		return "/xml/hanirss";
+		// 참고 소스 https://blog.naver.com/ffiwe/221687137070
 	}
 
 }
